0dfb959b54279a68105edbdcb67a6caf58856e2f
Nguyễn Hưng Thịnh <nhtnokia@gmail.com>
2021-09-22 20:15:41 +0700

    [BE-6] Done API Create course for teacher

--- src/main/java/com/minhchieu/controller/CourseController.java
+++ src/main/java/com/minhchieu/controller/CourseController.java
@@ -2,19 +2,18 @@ package com.minhchieu.controller;
 
 import com.minhchieu.mapstruct.dto.CourseGetByIdDTO;
 import com.minhchieu.mapstruct.dto.CourseGetDTO;
+import com.minhchieu.mapstruct.dto.CoursePostDTO;
 import com.minhchieu.mapstruct.mapper.MapStructMapper;
 import com.minhchieu.model.Account;
 import com.minhchieu.model.Course;
+import com.minhchieu.model.Teacher;
 import com.minhchieu.orm.AccountRepository;
 import com.minhchieu.orm.CourseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.EntityNotFoundException;
 import javax.validation.Valid;
@@ -51,5 +50,26 @@ public class CourseController {
     public ResponseEntity<CourseGetDTO> courseById (@PathVariable(value = "id") Long id){
         CourseGetDTO course = mapStructMapper.courseToCourseGetDTO(courseRepository.findById(id).orElseThrow(()-> new EntityNotFoundException()));
         return new ResponseEntity<>(course, HttpStatus.OK);
+    }
+
+    @PostMapping("/course")
+    public ResponseEntity<?> create(@Valid @RequestBody CoursePostDTO request, Authentication authentication){
+        //Default value
+        int status = 0; //pending
+        String type = "";
+        Course course = new Course();
+        course.setName(request.getName());
+        course.setPriceMoney(request.getPriceMoney());
+        course.setStatus(status);
+        course.setType(type);
+        course.setTeacherCourse(accountRepository.findByEmail(authentication.getName()).getTeacher());
+        courseRepository.save(course);
+        CourseGetDTO courseGetDTO = mapStructMapper.courseToCourseGetDTO(course);
+        return ResponseEntity.status(HttpStatus.CREATED).body(Map.of(
+            "message", "Created course successfully, but this course need approve from administrator",
+            "status", "pending",
+            "course",courseGetDTO
+
+        ));
     }
 }
